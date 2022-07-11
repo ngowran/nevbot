3b087a496f1a876f86b14893e56b2c55b2aed027
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-07-11 17:23:35 +0000

    unload/reload tweak

--- main.py
+++ main.py
@@ -21,16 +21,19 @@ async def on_ready():
   await bot.change_presence(activity=discord.Game(name="Type !help to use me"))
 
 # cog unloader command
-@client.command()
+# cog unloader command
+@commands.has_any_role('administrator', 'admin')
+@bot.command()
 async def unload(ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
+  bot.unload_extension(f'cogs.{extension}')
   await ctx.send(f'{extension} successfully unloaded')
 
-# cog reloader command, unload then load extenion
-@client.command()
+# cog reloader command, unload then load extension
+@commands.has_any_role('administrator', 'admin')
+@bot.command()
 async def reload(ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
-  client.load_extension(f'cogs.{extension}')
+  bot.unload_extension(f'cogs.{extension}')
+  bot.load_extension(f'cogs.{extension}')
   await ctx.send(f'{extension} successfully re-loaded')
 
   
